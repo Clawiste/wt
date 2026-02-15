e7a263cb5475f2269216e96b123db9d7db9a9102
Jan Prokes <j.prokes@me.com>
2026-02-15 14:55:43 -0800

    Add tarball sha256 to formula

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -2,7 +2,7 @@ class Wt < Formula
   desc "Interactive git worktree manager"
   homepage "https://github.com/Clawiste/wt"
   url "https://github.com/Clawiste/wt/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 ""
+  sha256 "6fd7ce94cc31427deb5b30e96b1c3e385a6e683aa92d5fab9c4d1627bab929fb"
   license "MIT"
 
   depends_on xcode: ["15.0", :build]
